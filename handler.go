fc2b3f1c3d39b136a89def4b780e9246ea907838
Andrew1407 <endry1407@gmail.com>
2020-10-14 23:20:13 +0300

    done: implementation, implementation_test.go

--- handler.go
+++ handler.go
@@ -6,7 +6,7 @@ type ComputeHandler struct {
 	// TODO: Add necessary fields.
 }
 
-func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
-	return nil
-}
+// func (ch *ComputeHandler) Compute() error {
+// 	// TODO: Implement.
+// 	return nil
+// }
